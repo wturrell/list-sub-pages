ea65541d6ae542032a3ace5ef4b0b465550ebce6
William Turrell <william@wturrell.co.uk>
2015-01-15 22:41:18 +0000

    Bugfix: duplicate links in sidebar. Use Groups plugin to check user is allowed to see page.

--- views/Back-End/create_shortcode.php
+++ views/Back-End/create_shortcode.php
@@ -266,6 +266,11 @@ function ls_shortcode($atts)
 	    // 'attachments' is a bit of a misleading name - these are the actual sub pages
 		foreach($attachments as $attachment)
    	    	{
+		        if ( class_exists( Groups_Post_Access ) && ! Groups_Post_Access::user_can_read_post( $attachment->ID, get_current_user_id() ) ) {
+			        // skip any pages they're not allowed to view
+			        continue;
+		        }
+
 		        $class = 'page-'.$attachment->post_name;
 
 		        $pagename = get_query_var( 'pagename' );
